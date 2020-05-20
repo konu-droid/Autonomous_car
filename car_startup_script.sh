7632d541da8c2c5daad8587c2663d3f9449c21a1
KVS Mohan Vamsi <kvs06051998@gmail.com>
2020-05-20 15:17:01 +0530

    first commit

--- car_startup_script.sh
+++ car_startup_script.sh
@@ -1,5 +1,4 @@
-sleep 1m;gnome-terminal --geometry=150x50 
---tab --title="echo" -e "bash -c \"echo "hello";echo "there";exec bash\"" 
+sleep 0.01m;gnome-terminal --geometry=150x50 --tab --title="echo" -e "bash -c \"echo "hello";echo "there";exec bash\"" 
 --tab --title="idea" -e "bash -c \"/opt/idea-IU-111.69/bin/idea.sh;exec bash\"" 
 --tab --title="sql" -e "bash -c \"mysql -uroot -pigdefault;\"" 
 --tab --title="firefox" -e "bash -c \"/usr/bin/firefox www.gmail.com;\""
